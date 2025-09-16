cd5ce5c050973766be5f456014d29f2face41a44
Dario Ureña <darioaxel@gmail.com>
2025-09-16 12:48:41 +0200

    actualizadas unidades 1 y 2

---
+++ src/.vuepress/.temp/pages/docencia/dam-daw/DWES/Prácticas/index.html.js
@@ -0,0 +1,16 @@
+import comp from "/home/darioaxel/Proyectos/darioaxel.github.io/src/.vuepress/.temp/pages/docencia/dam-daw/DWES/Prácticas/index.html.vue"
+const data = JSON.parse("{\"path\":\"/docencia/dam-daw/DWES/Pr%C3%A1cticas/\",\"title\":\"Prácticas\",\"lang\":\"es-ES\",\"frontmatter\":{\"title\":\"Prácticas\",\"article\":false,\"feed\":false,\"sitemap\":false,\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"Prácticas\\\"}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-docs-demo.netlify.app/docencia/dam-daw/DWES/Pr%C3%A1cticas/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"darioaxel\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Prácticas\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"es-ES\"}]]},\"readingTime\":{\"minutes\":0,\"words\":1},\"filePathRelative\":null,\"excerpt\":\"\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
