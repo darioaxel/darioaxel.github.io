47751f12622f194512da6be729ebd741b1d91b72
Dario Ureña <darioaxel@gmail.com>
2025-09-23 10:43:30 +0200

    solucionado contenidos extra en créditos

--- src/.vuepress/.temp/pages/docencia/dam-daw/DWES/98-creditos-reconocimientos.html.vue
+++ src/.vuepress/.temp/pages/docencia/dam-daw/DWES/98-creditos-reconocimientos.html.vue
@@ -7,13 +7,8 @@
 <h2 id="licencia-de-uso" tabindex="-1"><a class="header-anchor" href="#licencia-de-uso"><span>Licencia de uso</span></a></h2>
 <p>Este repositorio y todo su contenido está licenciado bajo licencia <strong>Creative Commons</strong>.Por favor si compartes, usas o modificas este proyecto cita a su<br>
 autor, y usa las mismas condiciones para su uso docente, formativo o educativo y no comercial.</p>
-<p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"><br>
-JoseLuisGS</span><br>
-by <a xmlns:cc="http://creativecommons.org/ns#" href="https://joseluisgs.dev/" property="cc:attributionName" rel="cc:attributionURL"><br>
-José Luis González Sánchez</a> is licensed under<br>
-a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons<br>
-Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional License</a>.<br />Creado a partir de la obra<br>
-en <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/joseluisgs" rel="dct:source"><a href="https://github.com/joseluisgs" target="_blank" rel="noopener noreferrer">https://github.com/joseluisgs</a></a>.</p>
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+<img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
 </div></template>
 
 
--- src/docencia/dam-daw/DWES/98-creditos-reconocimientos.md
+++ src/docencia/dam-daw/DWES/98-creditos-reconocimientos.md
@@ -14,10 +14,6 @@ No puedo dejar de dar mi más sincero agradecimiento y reconocimiento a todos lo
 Este repositorio y todo su contenido está licenciado bajo licencia **Creative Commons**.Por favor si compartes, usas o modificas este proyecto cita a su
 autor, y usa las mismas condiciones para su uso docente, formativo o educativo y no comercial.
 
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">
-JoseLuisGS</span>
-by <a xmlns:cc="http://creativecommons.org/ns#" href="https://joseluisgs.dev/" property="cc:attributionName" rel="cc:attributionURL">
-José Luis González Sánchez</a> is licensed under
-a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons
-Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional License</a>.<br />Creado a partir de la obra
-en <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/joseluisgs" rel="dct:source">https://github.com/joseluisgs</a>.
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+<img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+
