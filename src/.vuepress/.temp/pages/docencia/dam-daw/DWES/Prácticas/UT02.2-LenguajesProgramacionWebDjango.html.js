6c7b0ad094270e190680b9d1146532fca2a1e913
Dario Ureña <darioaxel@gmail.com>
2025-10-01 17:12:50 +0200

    actualizados los archivos de prácticas (en construcción)

---
+++ src/.vuepress/.temp/pages/docencia/dam-daw/DWES/Prácticas/UT02.2-LenguajesProgramacionWebDjango.html.js
@@ -0,0 +1,16 @@
+import comp from "/home/darioaxel/Proyectos/darioaxel.github.io/src/.vuepress/.temp/pages/docencia/dam-daw/DWES/Prácticas/UT02.2-LenguajesProgramacionWebDjango.html.vue"
+const data = JSON.parse("{\"path\":\"/docencia/dam-daw/DWES/Pr%C3%A1cticas/UT02.2-LenguajesProgramacionWebDjango.html\",\"title\":\"Práctica UT02.2 LPW. Django\",\"lang\":\"es-ES\",\"frontmatter\":{\"title\":\"Práctica UT02.2 LPW. Django\",\"date\":\"2027-05-10T00:00:00.000Z\",\"icon\":\"pen\",\"description\":\"Práctica UT002.2 Lenguajes de Programacion Web. Django Información Resultados de aprendizaje a trabajar RA2: Escribe sentencias ejecutables por un servidor Web reconociendo y ap...\",\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"Práctica UT02.2 LPW. Django\\\",\\\"image\\\":[\\\"\\\"],\\\"datePublished\\\":\\\"2027-05-10T00:00:00.000Z\\\",\\\"dateModified\\\":null,\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"darioaxel\\\",\\\"url\\\":\\\"darioaxel.dev\\\"}]}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-docs-demo.netlify.app/docencia/dam-daw/DWES/Pr%C3%A1cticas/UT02.2-LenguajesProgramacionWebDjango.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"darioaxel\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Práctica UT02.2 LPW. Django\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"Práctica UT002.2 Lenguajes de Programacion Web. Django Información Resultados de aprendizaje a trabajar RA2: Escribe sentencias ejecutables por un servidor Web reconociendo y ap...\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"es-ES\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2027-05-10T00:00:00.000Z\"}]]},\"readingTime\":{\"minutes\":0.72,\"words\":217},\"filePathRelative\":\"docencia/dam-daw/DWES/Prácticas/UT02.2-LenguajesProgramacionWebDjango.md\",\"excerpt\":\"\\n<table>\\n<thead>\\n<tr>\\n<th>Módulo y curso</th>\\n</tr>\\n</thead>\\n<tbody>\\n<tr>\\n<td><strong>Desarrollo Web en Entorno Servidor</strong></td>\\n</tr>\\n<tr>\\n<td><strong>Curso 2025/2026</strong></td>\\n</tr>\\n</tbody>\\n</table>\\n<div class=\\\"hint-container info\\\">\\n<p class=\\\"hint-container-title\\\">Información</p>\\n<p><strong>Resultados de aprendizaje a trabajar</strong></p>\\n<ul>\\n<li>RA2: Escribe sentencias ejecutables por un servidor Web reconociendo y aplicando procedimientos de integración del código en lenguajes de marcas.</li>\\n<li>RA3. Escribe bloques de sentencias embebidos en lenguajes de marcas, seleccionando y utilizando las estructuras de programación.</li>\\n</ul>\\n</div>\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
