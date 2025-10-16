96ef88ad11a9f5a46e8faf687bfade04b3bac275
Dario Ureña <darioaxel@gmail.com>
2025-10-16 11:47:30 +0200

    actualizados ejercicios

---
+++ src/.vuepress/.temp/pages/docencia/dam-daw/DWES/Ejercicios/.Tutoria01.html.js
@@ -0,0 +1,16 @@
+import comp from "/home/darioaxel/Proyectos/darioaxel.github.io/src/.vuepress/.temp/pages/docencia/dam-daw/DWES/Ejercicios/.Tutoria01.html.vue"
+const data = JSON.parse("{\"path\":\"/docencia/dam-daw/DWES/Ejercicios/.Tutoria01.html\",\"title\":\"Tutoría 1:\",\"lang\":\"es-ES\",\"frontmatter\":{\"date\":\"2024-09-01T00:00:00.000Z\",\"icon\":\"rocket\",\"description\":\"Tutoría 1: a. Introducción al VDI. b. Prueba de publicación de una página web en Apache 1. Estructura de ficheros 2. Posibles problemas con permisos 3. Copiar entre directorios ...\"},\"readingTime\":{\"minutes\":0.2,\"words\":60},\"filePathRelative\":\"docencia/dam-daw/DWES/Ejercicios/.Tutoria01.md\",\"excerpt\":\"\\n<p>a. Introducción al VDI.<br>\\nb. Prueba de publicación de una página web en Apache<br>\\n1. Estructura de ficheros<br>\\n2. Posibles problemas con permisos<br>\\n3. Copiar entre directorios mediante comandos<br>\\nc. Herramientas de gestión de versiones<br>\\n1. Uso de github en VSCode<br>\\n2. Creación del perfil de usuario en github<br>\\n3. Clonando repositorios</p>\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
