6b2f66e5241f196487e9809dfaf5ab76b8b08ff3
Dario Ureña <darioaxel@gmail.com>
2025-10-29 15:14:36 +0100

    entrada blog

---
+++ src/.vuepress/.temp/pages/blog/article/python-errors.html.js
@@ -0,0 +1,16 @@
+import comp from "/home/darioaxel/Proyectos/darioaxel.github.io/src/.vuepress/.temp/pages/blog/article/python-errors.html.vue"
+const data = JSON.parse("{\"path\":\"/blog/article/python-errors.html\",\"title\":\"Conceptos de Python 🐍\",\"lang\":\"es-ES\",\"frontmatter\":{\"title\":\"Conceptos de Python 🐍\",\"date\":\"2025-10-28T00:00:00.000Z\",\"category\":[\"Blog\"],\"tag\":[\"Programacion\",\"Python\"],\"type\":\"article\",\"description\":\"🐍 Conceptos clave de Python que todo desarrollador debe dominar 1. Objetos mutables e inmutables En Python, todo es un objeto. Sin embargo, algunos objetos pueden modificarse (...\"},\"readingTime\":{\"minutes\":4.16,\"words\":1249},\"filePathRelative\":\"blog/article/python-errors.md\",\"excerpt\":\"\\n<h2>1. Objetos mutables e inmutables</h2>\\n<p>En Python, todo es un objeto. Sin embargo, algunos objetos <strong>pueden modificarse (mutables)</strong> y otros <strong>no (inmutables)</strong>.<br>\\nComprender esta diferencia es esencial para evitar errores al pasar datos entre funciones o modificar estructuras de datos.</p>\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
