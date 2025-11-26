3f668a5aade780377e9f6ec9a0a0deb73914bc81
Dario Ureña <darioaxel@gmail.com>
2025-11-26 13:26:02 +0100

    añadido articulo ids en django

---
+++ src/.vuepress/.temp/pages/blog/article/django-identificacion.html.js
@@ -0,0 +1,16 @@
+import comp from "/home/darioaxel/Proyectos/darioaxel.github.io/src/.vuepress/.temp/pages/blog/article/django-identificacion.html.vue"
+const data = JSON.parse("{\"path\":\"/blog/article/django-identificacion.html\",\"title\":\"Identificación de Elementos de Modelo en Django\",\"lang\":\"es-ES\",\"frontmatter\":{\"title\":\"Identificación de Elementos de Modelo en Django\",\"date\":\"2025-11-26T00:00:00.000Z\",\"category\":[\"Blog\"],\"tag\":[\"Django\",\"ORM\"],\"type\":\"article\",\"description\":\"Identificación de Elementos de Modelo en Django: IDs en URL vs. Hardcoded Cuando desarrollamos aplicaciones web con Django, una de las tareas más comunes es identificar registro...\"},\"readingTime\":{\"minutes\":3.83,\"words\":1149},\"filePathRelative\":\"blog/article/django-identificacion.md\",\"excerpt\":\"\\n<p>Cuando desarrollamos aplicaciones web con Django, una de las tareas más comunes es identificar registros específicos de nuestra base de datos dentro de las vistas. Existen múltiples estrategias para lograrlo, cada una con implicaciones diferentes en términos de seguridad, mantenibilidad y experiencia de usuario. Exploremos las diversas fórmulas disponibles.</p>\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
